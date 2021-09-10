b0ec00f329bb7e2dca649d465437aa5ed6774f7c
irawan09 <arykurniadiirawan@yahoo.co.id>
2021-09-10 22:17:24 +0200

    update image loader

--- app/src/main/java/com/electroshock/technicaltestandroid/Cell.kt
+++ app/src/main/java/com/electroshock/technicaltestandroid/Cell.kt
@@ -12,7 +12,13 @@ data class Cell(
 data class ImageData(
     var cardImage: String = "N/A",
     var cardImageWidth: String = "N/A",
-    var cardImageHeight: String = "N/A"
+    var cardImageHeight: String = "N/A",
+    var cardImageTitle: String = "N/A",
+    var cardImageTitleTextColor: String = "N/A",
+    var cardImageTitleTextSize: String = "N/A",
+    var cardImageDescription: String = "N/A",
+    var cardImageDescriptionTextColor: String = "N/A",
+    var cardImageDescriptionTextSize: String = "N/A"
 
 )
 

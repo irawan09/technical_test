6dd2236ed8a60405b7a834ae677369d68916d106
irawan09 <arykurniadiirawan@yahoo.co.id>
2021-09-12 19:37:48 +0200

    ViewModel and Model Test added

--- app/src/main/java/com/electroshock/technicaltestandroid/DataViewModel.kt
+++ app/src/main/java/com/electroshock/technicaltestandroid/DataViewModel.kt
@@ -1,9 +1,24 @@
 package com.electroshock.technicaltestandroid
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DataViewModel : ViewModel() {
+
+    var cardImage: String = "N/A"
+    var cardImageWidth: String = "N/A"
+    var cardImageHeight: String = "N/A"
+    var cardImageTitle: String = "N/A"
+    var cardImageTitleTextColor: String = "N/A"
+    var cardImageTitleTextSize: String = "N/A"
+    var cardImageDescription: String = "N/A"
+    var cardImageDescriptionTextColor: String = "N/A"
+    var cardImageDescriptionTextSize: String = "N/A"
+
+    var dataModel  = MutableLiveData<CardImageData>()
+
+
     init {
         Log.d("DataViewModel", "DataViewModel created!")
     }
---
+++ app/src/main/java/com/electroshock/technicaltestandroid/DataViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.electroshock.technicaltestandroid
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class DataViewModelFactory():ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(DataViewModel::class.java)){
+            return DataViewModel() as T
+        }
+        throw IllegalArgumentException ("UnknownViewModel")
+    }
+}

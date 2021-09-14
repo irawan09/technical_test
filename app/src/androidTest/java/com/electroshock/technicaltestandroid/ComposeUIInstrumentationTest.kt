ea2eecc5ab4d687eef365a83bc9af7d90a79bd61
irawan09 <arykurniadiirawan@yahoo.co.id>
2021-09-15 00:14:40 +0200

    alhamdulillah selesai tinggal tambahin jenis compose UI testnya

--- app/src/androidTest/java/com/electroshock/technicaltestandroid/ComposeUIInstrumentationTest.kt
+++ app/src/androidTest/java/com/electroshock/technicaltestandroid/ComposeUIInstrumentationTest.kt
@@ -2,7 +2,6 @@ package com.electroshock.technicaltestandroid
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 

2c7c464a03ab4c8d2257b692d6a85da9dab89b97
irawan09 <arykurniadiirawan@yahoo.co.id>
2021-09-14 19:59:56 +0200

    test viewModel added

--- app/src/androidTest/java/com/electroshock/technicaltestandroid/ComposeUIInstrumentationTest.kt
+++ app/src/androidTest/java/com/electroshock/technicaltestandroid/ComposeUIInstrumentationTest.kt
@@ -1,12 +1,15 @@
 package com.electroshock.technicaltestandroid
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +18,11 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ComposeUIInstrumentationTest {
+    @get: Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.electroshock.technicaltestandroid", appContext.packageName)
+    fun title_card(){
+//        composeTestRule.onAllNodesWithText()
     }
 }

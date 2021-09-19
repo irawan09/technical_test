db0a257ecf241a331da643ee85e59e041c644a2d
irawan09 <arykurniadiirawan@yahoo.co.id>
2021-09-19 19:21:55 +0200

    OkHttpCaching added

--- app/src/main/java/com/electroshock/technicaltestandroid/MainActivity.kt
+++ app/src/main/java/com/electroshock/technicaltestandroid/MainActivity.kt
@@ -52,33 +52,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContext(this)
-        if (isNetworkAvailable(applicationContext) == true){
-            parseJSON()
-        }else {
-            Toast.makeText(this@MainActivity, "Please turn on your internet Connection", Toast.LENGTH_LONG).show()
-            finish()
-        }
-    }
-
-    // function to check if the network is available
-    private fun isNetworkAvailable(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val nw = connectivityManager.activeNetwork ?: return false
-            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
-            return when {
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                //for other device how are able to connect with Ethernet
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                //for check internet over Bluetooth
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
-                else -> false
-            }
-        } else {
-            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-            return nwInfo.isConnected
-        }
+        parseJSON()
     }
 
     // this function using to observe the data from view model
@@ -375,6 +349,26 @@ class MainActivity : ComponentActivity() {
 
         fun getContext(): Context {
             return context
+        }
+
+        fun hasNetwork(context: Context): Boolean? {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val nw = connectivityManager.activeNetwork ?: return false
+                val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+                return when {
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    //for other device how are able to connect with Ethernet
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    //for check internet over Bluetooth
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                    else -> false
+                }
+            } else {
+                val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+                return nwInfo.isConnected
+            }
         }
     }
 
--- app/src/main/java/com/electroshock/technicaltestandroid/api/RetrofitServiceFactory.kt
+++ app/src/main/java/com/electroshock/technicaltestandroid/api/RetrofitServiceFactory.kt
@@ -1,13 +1,32 @@
 package com.electroshock.technicaltestandroid.api
 
+import com.electroshock.technicaltestandroid.MainActivity
 import com.google.gson.GsonBuilder
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitServiceFactory{
+
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val myCache = Cache(MainActivity.getContext().cacheDir, cacheSize)
+
+    val okHttpClient = OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (MainActivity.hasNetwork(MainActivity.getContext())!!)
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+            else
+                request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+            chain.proceed(request)
+        }
+        .build()
     fun createService(): DataService = Retrofit.Builder()
         .baseUrl("https://private-8ce77c-tmobiletest.apiary-mock.com/")
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .client(okHttpClient)
         .build()
         .create(DataService::class.java)
 }
